34c88e1d43c5bc8f0474e8ae9fd62975e06f013c
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-02-22 20:44:26 +0100

    create menu for an application

--- app/src/main/java/com/learnitgirl/myvoc/AddNewWordActivity.java
+++ app/src/main/java/com/learnitgirl/myvoc/AddNewWordActivity.java
@@ -1,9 +1,11 @@
 package com.learnitgirl.myvoc;
 
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddNewWordActivity extends AppCompatActivity {
 
@@ -16,16 +18,35 @@ public class AddNewWordActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setTitle("Add New Word");
-        toolbar.setSubtitle("Subtitile");
+    }
 
-//        toolbar.setLogo(R.drawable.);
-//        toolbar.setNavigationIcon();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            //Call some material design APIs here
-        } else {
-            //Implement this feature without material design
+        String msg = "";
+
+        switch (item.getItemId()) {
+            case R.id.action_learn:
+                msg = "Learn";
+                break;
+            case R.id.action_new_word:
+                msg = "New Word";
+                break;
+            case R.id.action_review:
+                msg = "Review";
+                break;
+            case R.id.action_settings:
+                msg = "Review";
+                break;
         }
+
+        Toast.makeText(this, msg + "clicked!", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
