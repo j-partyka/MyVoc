755a0eaa2b786094b86fcebfad9b195890765897
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-02-25 23:39:10 +0100

    add disableHint method, but it doesn't work properly

--- app/src/main/java/com/learnitgirl/myvoc/AddNewWordActivity.java
+++ app/src/main/java/com/learnitgirl/myvoc/AddNewWordActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddNewWordActivity extends AppCompatActivity {
+
+//    private EditText foreignEditText = (EditText) findViewById(R.id.foreignWordEditText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,18 @@ public class AddNewWordActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setTitle("Add New Word");
+
+    }
+
+    public void disableHint(View v) {
+        EditText foreignEditText = (EditText) findViewById(R.id.foreignWordEditText);
+
+        EditText nativeEditText = (EditText) findViewById(R.id.nativeWordEditText);
+
+        if(v == foreignEditText){
+            foreignEditText.setHint("");
+        }else if(v == nativeEditText);
+            nativeEditText.setHint("");
     }
 
     @Override
@@ -45,8 +61,10 @@ public class AddNewWordActivity extends AppCompatActivity {
                 msg = "Settings";
                 break;
         }
-        
+
         Toast.makeText(this, msg + " clicked!", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
+
+
 }
