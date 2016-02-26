9d65adfe472b459407f9acde6a349c9f8511a34f
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-02-26 16:51:33 +0100

    add insert method

--- app/src/main/java/com/learnitgirl/myvoc/database/DictionaryContract.java
+++ app/src/main/java/com/learnitgirl/myvoc/database/DictionaryContract.java
@@ -11,7 +11,6 @@ public final class DictionaryContract {
 
     public static abstract class DictionaryEntry implements BaseColumns{
         public static final String TABLE_NAME = "dictionary";
-        public static final String COLUMN_NAME_WORD_ID = "wordID";
         public static final String COLUMN_NAME_FOREIGN_WORD = "foreignWord";
         public static final String COLUMN_NAME_NATIVE_WORD = "nativeWord";
         public static final String COLUMN_NAME_KNOWLEDGE = "knowledge";
--- app/src/main/java/com/learnitgirl/myvoc/database/DictionaryDBHelper.java
+++ app/src/main/java/com/learnitgirl/myvoc/database/DictionaryDBHelper.java
@@ -1,5 +1,6 @@
 package com.learnitgirl.myvoc.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,7 +19,6 @@ public class DictionaryDBHelper extends SQLiteOpenHelper{
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + DictionaryContract.DictionaryEntry.TABLE_NAME + " (" +
                     DictionaryContract.DictionaryEntry._ID + " INTEGER PRIMARY KEY," +
-                    DictionaryContract.DictionaryEntry.COLUMN_NAME_WORD_ID + TEXT_TYPE + COMMA_SEP +
                     DictionaryContract.DictionaryEntry.COLUMN_NAME_FOREIGN_WORD + TEXT_TYPE + COMMA_SEP +
                     DictionaryContract.DictionaryEntry.COLUMN_NAME_NATIVE_WORD + TEXT_TYPE + COMMA_SEP +
                     DictionaryContract.DictionaryEntry.COLUMN_NAME_KNOWLEDGE + TEXT_TYPE + COMMA_SEP + " )";
@@ -30,14 +30,43 @@ public class DictionaryDBHelper extends SQLiteOpenHelper{
         super(context, name, factory, version);
     }
 
+    /**
+     * Create table in database
+     * @param db
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
     }
 
+
+    /**
+     * Upgrades table in database
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
     }
+
+    public boolean insert(String foreignWord, String nativeWord, String knowledge){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(DictionaryContract.DictionaryEntry.COLUMN_NAME_FOREIGN_WORD, foreignWord);
+        values.put(DictionaryContract.DictionaryEntry.COLUMN_NAME_NATIVE_WORD, nativeWord);
+        values.put(DictionaryContract.DictionaryEntry.COLUMN_NAME_KNOWLEDGE, knowledge);
+
+        long newRowId = db.insert(DictionaryContract.DictionaryEntry.TABLE_NAME, null, values);
+
+        if (newRowId != -1)
+            return true;
+        else{
+            return false;
+        }
+    }
+
 }
