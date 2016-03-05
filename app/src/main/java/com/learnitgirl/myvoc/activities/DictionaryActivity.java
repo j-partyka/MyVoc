3e8c27cf8973cdbbf8bbb890b3e1eefa13a0a846
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-03-05 12:10:35 +0100

    correct reading from database, view database items

--- app/src/main/java/com/learnitgirl/myvoc/activities/DictionaryActivity.java
+++ app/src/main/java/com/learnitgirl/myvoc/activities/DictionaryActivity.java
@@ -1,23 +1,21 @@
 package com.learnitgirl.myvoc.activities;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import com.learnitgirl.myvoc.R;
-import com.learnitgirl.myvoc.utils.Constants;
-import com.learnitgirl.myvoc.utils.ListViewAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.learnitgirl.myvoc.database.DictionaryContract;
+import com.learnitgirl.myvoc.database.DictionaryDBHelper;
 
 public class DictionaryActivity extends AppCompatActivity {
-
-    private ArrayList<HashMap<String, String>> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,34 +28,28 @@ public class DictionaryActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.title_activity_dictionary);
 
         ListView listView = (ListView) findViewById(R.id.dictionary_listview);
-        list = new ArrayList<>();
 
-        HashMap<String, String> temp1 = new HashMap<>();
-        HashMap<String, String> temp2 = new HashMap<>();
-        HashMap<String, String> temp3 = new HashMap<>();
+        String[] from = {DictionaryContract.DictionaryEntry.COLUMN_NAME_FOREIGN_WORD,
+                DictionaryContract.DictionaryEntry.COLUMN_NAME_NATIVE_WORD,
+                DictionaryContract.DictionaryEntry.COLUMN_NAME_KNOWLEDGE};
 
-        temp1.put(Constants.FIRST_COLUMN, "door");
-        temp1.put(Constants.SECOND_COLUMN, "drzwi");
-        temp1.put(Constants.THIRD_COLUMN, "new");
-        list.add(temp1);
+        DictionaryDBHelper dbHelper = new DictionaryDBHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        temp2.put(Constants.FIRST_COLUMN, "door2");
-        temp2.put(Constants.SECOND_COLUMN, "drzwi2");
-        temp2.put(Constants.THIRD_COLUMN, "new2");
-        list.add(temp2);
+        int[] toViews = {R.id.foreignWordTextView, R.id.nativeWordTextView, R.id.knowledgeTextView};
 
-        temp3.put(Constants.FIRST_COLUMN, "door3");
-        temp3.put(Constants.SECOND_COLUMN, "drzwi3");
-        temp3.put(Constants.THIRD_COLUMN, "new3");
-        list.add(temp3);
+        String[] args = {};
 
-        ListViewAdapter adapter = new ListViewAdapter(this, list);
-        listView.setAdapter(adapter);
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DictionaryContract.DictionaryEntry.TABLE_NAME, args);
+        cursor.moveToFirst();
+        SimpleCursorAdapter simpleAdapter = new SimpleCursorAdapter(this, R.layout.row, cursor, from, toViews, 0);
+
+        listView.setAdapter(simpleAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(DictionaryActivity.this, position+1 + "clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(DictionaryActivity.this, position + 1 + "clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
