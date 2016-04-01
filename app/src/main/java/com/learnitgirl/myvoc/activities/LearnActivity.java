74e6a414cbbc2781feda5e965f4e780b0b80d565
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-04-01 17:51:37 +0200

    attempt to add increaseKnowledge method, which would increase int knowledge after every positive answer.

--- app/src/main/java/com/learnitgirl/myvoc/activities/LearnActivity.java
+++ app/src/main/java/com/learnitgirl/myvoc/activities/LearnActivity.java
@@ -73,6 +73,7 @@ public class LearnActivity extends AppCompatActivity {
 
         if (dbHelper.checkForeignWord(shownWord.getText().toString(), guessWord.getText().toString())) {
             Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+            dbHelper.increaseKnowledge(shownWord.getText().toString());
             guessWord.setText("");
             shownWord.setText(dbHelper.getRandomForeignWord());
 
